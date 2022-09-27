cef7369609288c1e1053cda1968519c7e13d88cd
Júnior Matos <sk8.merp@gmail.com>
2022-09-27 17:00:41 -0300

    feat: base do componente header criada

---
+++ Components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './style.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+        <p>ir para o site</p>
+    </header>
+  )
+}
+
+export default Header
