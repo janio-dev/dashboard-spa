c49b0b8278cc236b32f8c02ccab5e423fd52ecd7
Júnior Matos <sk8.merp@gmail.com>
2022-09-28 16:56:30 -0300

    feat: component theme global adicionado ao main

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../Hooks/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
 }
 
 export default MyApp
